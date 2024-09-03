01e2ff577f4fd25d8f6df071661b768e647e0e88
Allan Oliveira <allanolivei@gmail.com>
2024-09-03 12:29:59 -0300

    chore: change gtag

--- plugins/gtag.js
+++ plugins/gtag.js
@@ -3,7 +3,7 @@ import VueGtag from "vue-gtag";
 
 Vue.use(VueGtag, {
   config: {
-    id: "G-XYC2EWGZZ3",
+    id: "G-VRC5VJW3PB",
     // eslint-disable-next-line no-undef
     enabled: process.env.NUXT_ENV_LOCAL === undefined,
   },
